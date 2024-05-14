5860030a7a4982e850dc5de3f62d0d856eeeae14
Joseph McCartney <joseph.m.mccartney@spiritaero.com>
2024-05-14 13:40:14 -0500

    lots of cleanup, working version

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,17 @@
-#[allow(unused_extern_crates)]
-use clipboard_win::{formats, get_clipboard, set_clipboard};
-use regex::Regex;
+use clipboard_win::{formats, set_clipboard};
 use std::borrow::Borrow;
 use std::env;
-use std::io::Error;
 use std::path::Path;
-use std::slice;
 use windows::{core::Result, Win32::NetworkManagement::WNet};
 
 fn main() {
-    // Debugging only :P
-    // set_clipboard(formats::Unicode, "\"S:\\SMA Tasks\\boeing_ti_testing_tie_rods\\1. Test Plan\\MAA7-72472-5 Rev New_2024-05-10_INITIAL_DRAFT.docx\"").expect("To set clipboard");
-
-    // Get the input from the user
-    let input: String = get_clipboard(formats::Unicode).expect("To get clipboard");
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
-
-    // Strip quotes at the start and end (copy as path appends these)
-    let re = Regex::new("(^\"|\"$)").unwrap();
-    let input = re.replace_all(&input, "");
+    let input = &args[1]; // First arg is the path to *this* exe
 
     // Create a path object
     let path = Path::new::<str>(input.borrow());
 
     // Check if the path is valid
-    // todo: move these to a function
     if !path.exists() {
         println!("Path does not exist");
         return;
@@ -37,10 +23,6 @@ fn main() {
     }
 
     let _ = get_connection(&path);
-}
-
-fn print_type_of<T>(_: &T) {
-    println!("{}", std::any::type_name::<T>());
 }
 
 fn get_connection(input: &Path) -> Result<()> {
@@ -68,37 +50,33 @@ fn get_connection(input: &Path) -> Result<()> {
 
     // I'm lazy, I could either find the actual length, or just call this twice and be given
     // the length.
-    let result = unsafe { WNet::WNetGetConnectionW(root_ptr, remote_name_ptr, &mut length) };
+    let _ = unsafe { WNet::WNetGetConnectionW(root_ptr, remote_name_ptr, &mut length) };
     let result = unsafe { WNet::WNetGetConnectionW(root_ptr, remote_name_ptr, &mut length) };
 
     if result.is_err() {
-        println!("Not OK:\n{:?}", result.to_hresult().message());
-        return Err(result.into());
+        // we're *probably* a local path, just put the contents back on the clipboard
+        set_clipboard(
+            formats::Unicode,
+            &input.to_str().expect("already a good path/string"),
+        )
+        .expect("To set clipboard");
+        return Ok(());
     }
 
     // `remote_name` should have been updated with the remote name via the `lpremotename` pointer
     // `length` should have been updated with the length of `remote_name` if buffer is not large enough
+    // Convert to string
     length -= 1; // Avoid the null ternmination
     let output = String::from_utf16(&remote_name[..length as usize])
         .expect("Our bytes should be valid utf16");
 
+    // Replace the root with the connection name
     let universal_path = &input
         .to_str()
         .expect("I already know this is a good path")
         .replace(&root, &output);
 
     set_clipboard(formats::Unicode, universal_path.clone()).expect("To set clipboard");
-
-    // let asdf = universal_path
-    //     .encode_utf16()
-    //     .chain([0u16])
-    //     .collect::<Vec<u16>>();
-    // let asdf = String::from_utf16(&asdf);
-    // println!("{:?}", asdf);
-
-    // let blah = asdf.iter().map(|x| char::from_digit(x).unwrap());
-
-    println!("{}", universal_path);
 
     Ok(())
 }
