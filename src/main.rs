853fc5dfc09ec80ad95edd5b0993bc54b9b78267
Joseph McCartney <joseph.m.mccartney@spiritaero.com>
2024-05-15 07:20:41 -0500

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use windows::{core::Result, Win32::NetworkManagement::WNet};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let input = &args[1]; // First arg is the path to *this* exe
+    let input = &args[1]; // First arg is the path to *this* program
 
     // Create a path object
     let path = Path::new::<str>(input.borrow());
@@ -36,14 +36,14 @@ fn get_connection(input: &Path) -> Result<()> {
 
     // [in] `lplocalname`: Pointer to a constant null-terminated string that specifies the name of the
     // local device to get the network name for
-    let mut v = root.encode_utf16().chain([0u16]).collect::<Vec<u16>>();
-    let root_ptr = windows::core::PWSTR(v.as_mut_ptr());
+    let mut root_u16_buffer = root.encode_utf16().chain([0u16]).collect::<Vec<u16>>();
+    let root_ptr = windows::core::PWSTR(root_u16_buffer.as_mut_ptr());
 
     // [out] `lpremotename`: Pointer to a null-terminated string that receives the remote name used
     // to make the connection
     static BUFFER_SIZE: u32 = 255;
-    let mut remote_name = vec![0_u16; BUFFER_SIZE as usize];
-    let remote_name_ptr = windows::core::PWSTR(remote_name.as_mut_ptr());
+    let mut remote_name_buffer = vec![0_u16; BUFFER_SIZE as usize];
+    let remote_name_ptr = windows::core::PWSTR(remote_name_buffer.as_mut_ptr());
 
     // [in, out] `lpnlength`: Pointer to a variable that specivies the size of the buffer pointed
     // to by the `lpremotename` parameter, in characters.  If the function fails because the buffer
@@ -51,7 +51,10 @@ fn get_connection(input: &Path) -> Result<()> {
     let mut length: u32 = BUFFER_SIZE;
 
     let result = unsafe { WNet::WNetGetConnectionW(root_ptr, remote_name_ptr, &mut length) };
-    length = remote_name.iter().position(|x| *x == 0u16).unwrap_or(0) as u32;
+    length = remote_name_buffer
+        .iter()
+        .position(|x| *x == 0u16)
+        .unwrap_or(0) as u32;
 
     if result.is_err() {
         // we're *probably* a local path, just put the contents back on the clipboard
@@ -67,16 +70,16 @@ fn get_connection(input: &Path) -> Result<()> {
     // `length` should have been updated with the length of `remote_name` if buffer is not large enough
     // Convert to string
     length -= 1; // Avoid the null ternmination
-    let output = String::from_utf16(&remote_name[..length as usize])
+    let remote_name = String::from_utf16(&remote_name_buffer[..length as usize])
         .expect("Our bytes should be valid utf16");
-    
 
     // Replace the root with the connection name
     let universal_path = &input
         .to_str()
         .expect("I already know this is a good path")
-        .replace(&root, &output);
+        .replace(&root, &remote_name);
 
+    // finally, place the new path on the clipboard
     set_clipboard(formats::Unicode, universal_path.clone()).expect("To set clipboard");
 
     Ok(())
